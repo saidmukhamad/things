2ddeeea9c928e761617510788422ba854d888583
saidmukhamad <saidmukhamad@yandex.ru>
2022-02-23 02:00:42 +0300

    Feedback addition

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { useState } from "react";
+import RatingSelect from "./RatingSelect";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
-function FeedbackForm() {
-  const [text, setText] = useState();
+function FeedbackForm({ handleAdd }) {
+  const [text, setText] = useState("");
+  const [rating, setRating] = useState(10);
+  const [btnDisabled, setbtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
   const handleTextChange = (e) => {
+    if (text === "") {
+      setbtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setbtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setMessage(null);
+      setbtnDisabled(false);
+    }
+
     setText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback);
+
+      setText("");
+      setRating(10);
+    }
   };
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would your rate your service?</h2>
-        {/* TODO: rating select components */}
+        <RatingSelect
+          select={(rating) => {
+            setRating(rating);
+          }}
+        />
         <div className="input-group">
           <input
             onChange={handleTextChange}
@@ -19,8 +55,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <button type="submit">Send</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
